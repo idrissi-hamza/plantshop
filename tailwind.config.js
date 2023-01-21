4c6ff33e9309c404183f25251cfc690ddaedf98c
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-01-21 22:45:18 +0100

    add nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     // Or if using `src` directory:
     // "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-nunito)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
