a7013db758c28c3f8ce775c65af78208e12a278a
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-01-21 22:30:47 +0100

    display components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,34 @@
-import { Inter } from '@next/font/google';
-import Layout from '@/components/Layout';
-
-const inter = Inter({ subsets: ['latin'] });
+import Card from "../components/Card";
+import Layout from "../components/Layout";
+import data from "../utils/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Layout>
-      <main className=''>
-        <div className="bg-red-300">h</div>
-      </main>
+      <div className="min-h-[70vh] mx-auto">
+        <h1>Products</h1>
+        <ul
+          className="gap-10 g-red-300 
+        
+        my-10 
+        grid 
+        md:grid-cols-2  lg:grid-cols-3
+         2xl:grid-cols-4 3xl:flex flex-wrap justify-center min-h-full space"
+        >
+          {data.plants.map((plant, i) => (
+            <li key={plant.slug}>
+              <Card key={i} plant={plant} />
+            </li>
+          ))}
+        </ul>
+        {/* <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <Card/>
+      </ul> */}
+      </div>
     </Layout>
   );
 }
