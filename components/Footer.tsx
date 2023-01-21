c2fdf089073391282fe5cf7379acd5344aa3efa3
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-01-21 21:19:39 +0100

    create footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-center space-y-2 mt-24 p-4">
+      <p>Copyright &copy; Eshop 2022</p>
+      {/* <p>
+        <Link href="/about">
+          <a>About Eshop </a>
+        </Link>
+      </p> */}
+    </footer>
+  );
+};
+
+export default Footer;
