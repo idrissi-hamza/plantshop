b7af1c23fa47393b5b8feca634867b0aa089ecd2
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-01-21 21:19:50 +0100

    create layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Footer from './Footer';
+import Header from './Header';
+
+const Layout = ({
+  title,
+  keywords,
+  description,
+  children,
+}: {
+  title: string;
+  keywords: string;
+  description: string;
+  children: JSX.Element;
+}) => {
+  return (
+    <div className="min-h-screen flex flex-col max-w-8xl mx-auto ">
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="description"
+          content={description}
+        />
+        <meta
+          name="keywords"
+          content={keywords}
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+Layout.defaultProps = {
+  title: 'shop Market | Find your product',
+  description: 'Find your product',
+  keywords: 'product1,product2',
+  // TODO to custom later
+};
+
+export default Layout;
