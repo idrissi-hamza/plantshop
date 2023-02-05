6c733fe5b380ff803b30dfb06bde675ae8941d6e
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-02-05 16:50:51 +0100

    update payRadio

--- components/PayRadio.tsx
+++ components/PayRadio.tsx
@@ -1,10 +1,10 @@
 import { Field } from 'formik';
-import { SlPaypal,SlCreditCard } from 'react-icons/sl';
+import { SlPaypal, SlCreditCard } from 'react-icons/sl';
 import { BsCashCoin } from 'react-icons/bs';
 const PayRadio = ({ label, options, ...props }: any) => {
   return (
-    <div className="mb-5  w-full font-display flex flex-col gap-4 ">
-      <label className="block mb-2  font-bold text-gray-700  text-3xl">
+    <div className="pt-10 mb-5  w-full font-display flex flex-col gap-4 justify-center items-center">
+      <label className="block mb-6 bg-clip-text text-transparent bg-gradient-to-r to-blue-500 from-[#b2bc83]  font-bold   text-4xl">
         {label}
       </label>
       <ul className="flex flex-col gap-6">
@@ -39,12 +39,12 @@ const RadioButton = ({ field, form, ...props }: any) => {
         required
       />
 
-      <div className="inline-flex items-center justify-between gap-10 w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:peer-checked:text-blue-500 peer-checked:border-blue-600s peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 peer-checked:shadow-xl ">
-        <span className="w-full text-lg font-semibold">{props.label} </span>
-        {props.value === 'paypal' && <SlPaypal className="text-4xl"   />}
-        {props.value === 'stripe' && <SlCreditCard className="text-5xl"   />}
+      <div className="inline-flex items-center justify-between gap-10 w-full p-5 text-gray-500 font-bold border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600s peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 peer-checked:bg-gray-50 peer-checked:shadow-xl ">
+        <span className="w-full text-lg font-bold">{props.label} </span>
+        {props.value === 'paypal' && <SlPaypal className="text-4xl" />}
+        {props.value === 'stripe' && <SlCreditCard className="text-5xl" />}
         {props.value === 'cashOnDelivery' && (
-          <BsCashCoin className="text-5xl"   />
+          <BsCashCoin className="text-5xl" />
         )}
       </div>
     </label>
